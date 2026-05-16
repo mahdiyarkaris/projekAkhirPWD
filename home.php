2641b8dfdbeaa66df66fb96a1ec7e30457bb23f4
mahdiyaarkaris <mahdiyaarkaris@gmail.com>
2026-05-16 18:24:42 +0700

    menu navbar

--- home.php
+++ home.php
@@ -6,7 +6,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Dessert</title>
-    <link rel="stylesheet" href="ubahvera.css">
+    <link rel="stylesheet" href="ubah.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
   </head>
   <body>
@@ -20,10 +20,10 @@
       <div class="collapse navbar-collapse" id="navbarScroll">
         <ul class="navbar-nav mx-auto my-2 my-lg-0 navbar-nav-scroll" style="--bs-scroll-height: 100px;">
           <li class="nav-item">
-            <a class="nav-link active" href="home1.php">Home</a>
+            <a class="nav-link active" href="home.php">Home</a>
           </li>
           <li class="nav-item">
-            <a class="nav-link" href="menu1.php">Menu</a>
+            <a class="nav-link" href="menu.php">Menu</a>
           </li>
           <li class="nav-item">
             <a class="nav-link" href="about.php">About</a>
