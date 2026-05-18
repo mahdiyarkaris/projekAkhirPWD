1c77592125f1ad7f39414c7bb660828d96b2424a
deavera <deavera18@gmail.com>
2026-05-18 22:40:10 +0700

    login

---
+++ login.php
@@ -0,0 +1,104 @@
+<?php
+session_start();
+include 'koneksi.php';
+ 
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    $email    = $_POST['email'];
+    $password = $_POST['password'];
+ 
+    $query = mysqli_query($koneksi, "SELECT * FROM users WHERE email = '$email'");
+    $data  = mysqli_fetch_array($query);
+ 
+    if ($data && password_verify($password, $data['password'])) {
+        $_SESSION['logged_in'] = true;
+        $_SESSION['id'] = $data['id'];
+        $_SESSION['nama'] = $data['nama'];
+        $_SESSION['role'] = $data['role'];
+        $_SESSION['email'] = $data['email'];
+        $_SESSION['no_hp'] = $data['no_hp'];
+        $_SESSION['alamat'] = $data['alamat'];
+ 
+        if ($data['role'] == 'admin') {
+            header('location: admin.php');
+        } else {
+            header('location: menu1.php');
+        }
+        exit();
+    } else {
+        $error = "Email atau password salah!";
+    }
+}
+?>
+
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Dessert</title>
+    <link rel="stylesheet" href="ubahvera.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
+  </head>
+  <body>
+
+<nav class="navbar navbar-expand-lg">
+    <div class="container-fluid">
+      <a class="navbar-brand" href="#">Dessert</a>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarScroll" aria-controls="navbarScroll" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarScroll">
+        <ul class="navbar-nav mx-auto my-2 my-lg-0 navbar-nav-scroll" style="--bs-scroll-height: 100px;">
+          <li class="nav-item">
+            <a class="nav-link" href="home1.php">Home</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="menu1.php">Menu</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="about.php">About</a>
+          </li>
+        </ul>
+        <ul class="navbar-nav nav-right my-2 my-lg-0 navbar-nav-scroll" style="--bs-scroll-height: 100px;">
+          <li class="nav-item">
+            <a class="nav-link" href="register.php">Register</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+
+<section class="daftar">
+    <h1 class="welcome">Welcome to Dessert</h1>
+    <p class="deskripsi">We resigned our side to make shopping with us better!"</p>
+    <div class="card">
+        <h5 class="card-header text-center">Login</h5>
+        <div class="card-body">
+        <?php if (isset($error)): ?>
+            <div class="alert alert-danger"><?php echo $error; ?></div>
+        <?php endif; ?>
+          <form method="POST" action="login.php">
+            <div class="mb-3">
+                <label class="form-label">Email address</label>
+                <input type="email" class="form-control" name="email" required>
+            </div>
+            <div class="mb-3">
+                <label class="form-label">Password</label>
+                <input type="password" class="form-control" name="password" required>
+            </div>
+            <button type="submit" class="btn btn-primary">Login</button>
+          </form>
+            <p class="text-center mt-2">Don't have an account? <a href="register.php">Register here</a></p>
+        </div>
+    </div>
+</section>
+
+<footer class="footer">
+    <div class="container text-center">
+        <p>&copy; 2026 Dessert. All rights reserved.</p>
+    </div>
+</footer>
+
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
+  </body>
+</html>
